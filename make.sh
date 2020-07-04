07e6473d4a6b4d8c31532de09fde5da80872cc9e
Hongliang Wang <hongliangwang@yunify.com>
2020-07-04 09:01:57 +0800

    add pr check

--- make.sh
+++ make.sh
@@ -2,59 +2,81 @@
 
 set -eo pipefail
 
-fatal() {
-  echo ${@:2}
-  return $1
+main() {
+  if [ "$GITHUB_EVENT_NAME" == "pull_request" ];then
+    log "checking ..."
+    check
+  elif [ "$GITHUB_EVENT_NAME" == "push" ];then
+    log "building ..."
+    build
+    log "publishing new role packages ..."
+    deploy
+  fi
 }
 
 build() {
-  # find all roles
-  local roles="$(findRoles)"
-
-  # build packages
-  local role; for role in $roles; do
-    local version="$(awk '$1=="version:" {print $2}' $role/meta.yml)"
-    echo "creating package for $role-$version ..."
-    tar czf $role-${version:?missing}.tar.gz -C $role .
+  # build changed packages
+  local changedPackage; for changedPackage in $(findChangedPackageNames); do
+    log "creating package $changedPackage ..."
+    tar czf $changedPackage -C ${changedPackage%-*} .
   done
+}
 
+check() {
+  local changedPackages; changedPackages="$(findChangedPackageNames)"
+  local changedCount=$(echo $changedPackages | wc -w)
+  if [ "$changedCount" -gt 1 ]; then
+    fatal 1 "Only one package is allowed to update at a time, though found $changedCount changed packages: [${changedPackages// /, }]."
+  fi
+}
+
+deploy() {
   # switch to gh-pages branch
   git fetch
   git checkout -t origin/gh-pages
 
-  # ensure existing versions are unchanged
-  local changedPackages="$(git diff --name-only | grep ".tar.gz$")"
-  [ -z "$changedPackages" ] || fatal 1 "Changed roles should have versions updated together: $changedPackages."
-
-  # ensure new role versions are provided
-  local role; for role in $(findRoles); do
-    egrep -q "^version: [0-9]+\.[0-9]+\.[0-9]+" $role/meta/main.yml || fatal 1 "role version is required: [$role]."
-  done
-}
-
-deploy() {
-  echo "preparing git author info ..."
+  log "preparing git author info ..."
   git config --global user.name "github-actions[bot]"
   git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-  echo "preparing git commit ..."
+
+  log "preparing git commit ..."
   git add *.tar.gz
   git diff --staged --quiet || git commit -m "update"
-  echo "pushing changes if any ..."
+
+  log "pushing changes if any ..."
   git push
+}
+
+findChangedPackageNames() {
+  comm -13 <(findPublishedPackageNames | sort -V) <(findPendingPackageNames | sort -V) | xargs
+}
+
+findPublishedPackageNames() {
+  log "retrieving published packages ..."
+  local owner=${GITHUB_REPOSITORY%%/*}
+  local repo=${GITHUB_REPOSITORY##*/}
+  local url=https://$owner.github.io/$repo/index.yml
+  curl -sL $url | awk '$1=="-" {print $2}'
+}
+
+findPendingPackageNames() {
+  local role version; for role in $(findRoles); do
+    version="$(awk '$1=="role_version:" {print $2}' $role/meta.yml)"
+    echo $role-${version:?missing}.tar.gz
+  done
 }
 
 findRoles() {
    find . -mindepth 1 -maxdepth 1 -type d ! -name ".*" -printf "%f\n"
 }
 
-main() {
-  echo "building ..."
-  build
+fatal() {
+  log ${@:2}
+  return $1
+}
 
-  if [ "$GITHUB_EVENT_NAME" == "push" ];then
-    echo "publishing new role packages ..."
-    deploy
-  fi
+log() {
+  echo "$@" 1>&2
 }
 
 main
